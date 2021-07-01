48090e5394c23089f549721a45cc0139eaa0791d
MCXIV <mcxiv14@gmail.com>
2021-07-01 21:33:24 +0530

    Addition: TabButton

---
+++ src/main/java/everyos.browser.webicitybrowser/everyos/browser/webicitybrowser/gui/component/TabButton.java
@@ -0,0 +1,27 @@
+package everyos.browser.webicitybrowser.gui.component;
+
+import everyos.engine.ribbon.core.component.BlockComponent;
+import everyos.engine.ribbon.core.component.Component;
+import everyos.engine.ribbon.core.directive.FontSizeDirective;
+import everyos.engine.ribbon.core.graphics.InvalidationLevel;
+
+public class TabButton extends BlockComponent {
+	private String text;
+
+	public TabButton(Component parent) {
+		super();
+		
+		this
+			.directive(FontSizeDirective.of(14));
+	}
+
+	public Component text(String text) {
+		this.text = text;
+		invalidate(InvalidationLevel.RENDER);
+		return this;
+	}
+	
+	public String getText() {
+		return this.text;
+	}
+}
