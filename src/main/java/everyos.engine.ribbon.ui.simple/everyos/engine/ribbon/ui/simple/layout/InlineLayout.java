3b4d0699631dfcf1d6b9198578effa9d1e8e363f
MCXIV <mcxiv14@gmail.com>
2021-07-06 14:25:53 +0530

    Removed unrequired changes

--- src/main/java/everyos.engine.ribbon.ui.simple/everyos/engine/ribbon/ui/simple/layout/InlineLayout.java
+++ src/main/java/everyos.engine.ribbon.ui.simple/everyos/engine/ribbon/ui/simple/layout/InlineLayout.java
@@ -14,56 +14,46 @@ public class InlineLayout implements Layout {
 	private Component component;
 	private ComponentUI ui;
 	private ComputedChildrenHelper computedChildrenHelper;
-	private boolean considerChildren = true;
 
 	public InlineLayout(Component component, ComponentUI ui) {
 		this.component = component;
 		this.ui = ui;
 	}
 
-	public void setConsiderChildren(boolean considerChildren) {
-		this.considerChildren = considerChildren;
-	}
-
 	@Override
 	public void render(Renderer r, SizePosGroup sizepos, UIManager uimgr, Appearence appearence) {
 		appearence.render(r, sizepos, uimgr);
+		renderChildren(r, sizepos, uimgr);
 
-		// We don't check consideration in renderChildren so that implementor can create a custom system for the same.
-		if (considerChildren)
-			renderChildren(r, sizepos, uimgr);
 	}
 
 	@Override
 	public void paint(Renderer r, Appearence appearence) {
 		appearence.paint(r);
-
-		// We don't check consideration in paintChildren so that implementor can create a custom system for the same.
-		if (considerChildren)
-			paintChildren(r);
+		paintChildren(r);
 	}
 
 	@Override
 	public void processEvent(UIEvent event) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void directive(UIDirective directive) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	public void renderChildren(Renderer r, SizePosGroup sizepos, UIManager uimgr) {
+	private void renderChildren(Renderer r, SizePosGroup sizepos, UIManager uimgr) {
 		this.computedChildrenHelper = new ComputedChildrenHelper(this.component, c->uimgr.get(c, ui));
-		
+
 		for (ComponentUI c: computedChildrenHelper.getChildren()) {
 			c.render(r, sizepos, uimgr);
 		}
 	}
 
-	public void paintChildren(Renderer r) {
+	private void paintChildren(Renderer r) {
 		for (ComponentUI c: computedChildrenHelper.getChildren()) {
 			r.useBackground();
 			c.paint(r);
