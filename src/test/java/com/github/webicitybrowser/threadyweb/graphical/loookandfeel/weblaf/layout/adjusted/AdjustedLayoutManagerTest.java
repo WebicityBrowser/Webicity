68d8efa61d0f7c67a843c417c6daaec8c5561b68
Jason Gronn <jasontopia03@gmail.com>
2023-10-12 21:23:17 -0400

    Finish unit tests for relative position

--- src/test/java/com/github/webicitybrowser/threadyweb/graphical/loookandfeel/weblaf/layout/adjusted/AdjustedLayoutManagerTest.java
+++ src/test/java/com/github/webicitybrowser/threadyweb/graphical/loookandfeel/weblaf/layout/adjusted/AdjustedLayoutManagerTest.java
@@ -5,10 +5,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import com.github.webicitybrowser.thready.dimensions.AbsolutePosition;
 import com.github.webicitybrowser.thready.dimensions.AbsoluteSize;
 import com.github.webicitybrowser.thready.gui.directive.basics.pool.BasicDirectivePool;
 import com.github.webicitybrowser.thready.gui.directive.core.pool.DirectivePool;
 import com.github.webicitybrowser.thready.gui.graphical.layout.base.flowing.FlowingLayoutManager;
+import com.github.webicitybrowser.thready.gui.graphical.layout.core.ChildLayoutResult;
 import com.github.webicitybrowser.thready.gui.graphical.layout.core.LayoutManagerContext;
 import com.github.webicitybrowser.thready.gui.graphical.layout.core.LayoutResult;
 import com.github.webicitybrowser.thready.gui.graphical.layout.core.SolidLayoutManager;
@@ -16,10 +18,13 @@ import com.github.webicitybrowser.thready.gui.graphical.lookandfeel.core.stage.b
 import com.github.webicitybrowser.thready.gui.graphical.lookandfeel.core.stage.box.ChildrenBox;
 import com.github.webicitybrowser.thready.gui.graphical.lookandfeel.core.stage.render.GlobalRenderContext;
 import com.github.webicitybrowser.thready.gui.graphical.lookandfeel.core.stage.render.LocalRenderContext;
+import com.github.webicitybrowser.threadyweb.graphical.directive.layout.common.position.PositionOffsetDirective;
+import com.github.webicitybrowser.threadyweb.graphical.directive.layout.common.position.PositionTypeDirective;
 import com.github.webicitybrowser.threadyweb.graphical.lookandfeel.weblaf.layout.adjusted.AdjustedLayoutManager;
 import com.github.webicitybrowser.threadyweb.graphical.loookandfeel.test.TestStubBlockBox;
 import com.github.webicitybrowser.threadyweb.graphical.loookandfeel.test.TestStubContentBox;
 import com.github.webicitybrowser.threadyweb.graphical.loookandfeel.weblaf.layout.flow.FlowTestUtils;
+import com.github.webicitybrowser.threadyweb.graphical.value.PositionType;
 
 public class AdjustedLayoutManagerTest {
 
@@ -56,7 +61,27 @@ public class AdjustedLayoutManagerTest {
 			box, box.getChildrenTracker().getChildren(), globalRenderContext, localRenderContext));
 		Assertions.assertEquals(new AbsoluteSize(50, 10), result.fitSize());
 		Assertions.assertEquals(1, result.childLayoutResults().length);
-		
+		ChildLayoutResult childResult = result.childLayoutResults()[0];
+		Assertions.assertEquals(new AbsolutePosition(0, 0), childResult.relativeRect().position());
+	}
+
+	@Test
+	@DisplayName("Can render box with relative child box")
+	public void canRenderBoxWithRelativeChildBox() {
+		ChildrenBox box = new TestStubBlockBox(emptyDirectivePool);
+		DirectivePool childDirectivePool = new BasicDirectivePool();
+		childDirectivePool.directive(PositionTypeDirective.of(PositionType.RELATIVE));
+		childDirectivePool.directive(PositionOffsetDirective.ofLeft(_1 -> 5));
+		Box childBox = new TestStubContentBox(true, new AbsoluteSize(10, 10), childDirectivePool);
+		box.getChildrenTracker().addChild(childBox);
+		GlobalRenderContext globalRenderContext = FlowTestUtils.mockGlobalRenderContext();
+		LocalRenderContext localRenderContext = FlowTestUtils.createLocalRenderContext();
+		LayoutResult result = layoutManager.render(new LayoutManagerContext(
+			box, box.getChildrenTracker().getChildren(), globalRenderContext, localRenderContext));
+		Assertions.assertEquals(new AbsoluteSize(50, 10), result.fitSize());
+		Assertions.assertEquals(1, result.childLayoutResults().length);
+		ChildLayoutResult childResult = result.childLayoutResults()[0];
+		Assertions.assertEquals(new AbsolutePosition(5, 0), childResult.relativeRect().position());
 	}
 
 }
