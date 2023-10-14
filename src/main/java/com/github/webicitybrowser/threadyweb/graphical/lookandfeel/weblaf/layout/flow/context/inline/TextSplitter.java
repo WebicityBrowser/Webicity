c28ceb868112323b3235aae72fcfa663754736fc
Jason Gronn <jasontopia03@gmail.com>
2023-10-13 23:49:20 -0400

    Implement the CSS line-break property

--- src/main/java/com/github/webicitybrowser/threadyweb/graphical/lookandfeel/weblaf/layout/flow/context/inline/TextSplitter.java
+++ src/main/java/com/github/webicitybrowser/threadyweb/graphical/lookandfeel/weblaf/layout/flow/context/inline/TextSplitter.java
@@ -3,6 +3,7 @@ package com.github.webicitybrowser.threadyweb.graphical.lookandfeel.weblaf.layou
 import com.github.webicitybrowser.thready.dimensions.RelativeDimension;
 import com.github.webicitybrowser.thready.drawing.core.text.Font2D;
 import com.github.webicitybrowser.thready.drawing.core.text.FontMetrics;
+import com.github.webicitybrowser.threadyweb.graphical.directive.text.LineBreakDirective.LineBreak;
 
 
 public class TextSplitter {
@@ -10,14 +11,16 @@ public class TextSplitter {
 	private final String text;
 	private final Font2D font;
 	private final float letterSpacing;
+	private final LineBreak lineBreak;
 	private final float[] charWidths;
 
 	private int windowEnd = 0;
 
-	public TextSplitter(String text, Font2D font, float letterSpacing) {
+	public TextSplitter(String text, Font2D font, LineBreak lineBreak, float letterSpacing) {
 		this.text = text;
 		this.font = font;
 		this.letterSpacing = letterSpacing;
+		this.lineBreak = lineBreak;
 		this.charWidths = generateCharWidths();
 	}
 
@@ -28,6 +31,7 @@ public class TextSplitter {
 
 		float currentWidth = 0;
 		int windowStart = windowEnd;
+		int spacePosition = -1;
 		while (!completed() && ((forceFit && currentWidth == 0) || !nextCharWillOverflow(currentWidth, maxWidth))) {
 			if (currentWidth != 0) {
 				currentWidth += letterSpacing;
@@ -35,13 +39,25 @@ public class TextSplitter {
 			
 			currentWidth += charWidths[windowEnd];
 			windowEnd++;
+
+			if (windowEnd >= text.length() || text.charAt(windowEnd) == ' ') {
+				spacePosition = windowEnd;
+			}
+		}
+
+		if (!forceFit && spacePosition == -1 && lineBreak != LineBreak.ANYWHERE) {
+			windowEnd = windowStart;
+			return null;
 		}
 
 		if (windowStart == windowEnd) {
 			return null;
 		}
 
-		return text.substring(windowStart, windowEnd);
+		if (spacePosition == -1 || lineBreak == LineBreak.ANYWHERE) spacePosition = windowEnd;
+		windowEnd = spacePosition;
+
+		return text.substring(windowStart, spacePosition);
 	}
 
 	public boolean completed() {
