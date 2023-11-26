06969ff027469f28cd0342dcf919501e420dbc04
Jason Gronn <jasontopia03@gmail.com>
2023-11-25 23:41:23 -0500

    Work on documentation

--- src/main/java/com/github/webicitybrowser/thready/gui/graphical/lookandfeel/core/UIDisplay.java
+++ src/main/java/com/github/webicitybrowser/thready/gui/graphical/lookandfeel/core/UIDisplay.java
@@ -47,14 +47,52 @@ public interface UIDisplay<T extends Context, U extends Box, V extends RenderedU
 	 */
 	List<U> generateBoxes(T displayContext, BoxContext boxContext, StyleGenerator styleGenerator);
 
+	/**
+	 * Renders the given box into a unit that can be painted.
+	 * This will determine constraints such as sizes and
+	 * positions for this box and any children.
+	 * @param box the box to render
+	 * @param globalRenderContext a context containing data or
+	 * 	operations that may be useful for rendering the box
+	 * @param localRenderContext a context containing data determined
+	 *  by a parent box that may be useful for rendering the box
+	 */
 	V renderBox(U box, GlobalRenderContext globalRenderContext, LocalRenderContext localRenderContext);
 
+	/**
+	 * Assign composite layers to the given unit. This is
+	 * designed to make some operations, such as positioning,
+	 * easier to implement. It is also meant to allow animations
+	 * to be implemented efficiently.
+	 * @param unit the unit to composite
+	 * @param compositeContext a context containing data or
+	 * 	operations that may be useful for compositing the unit
+	 * @param localCompositeContext a context containing data
+	 * 	determined by a parent unit that may be useful for compositing
+	 * 	the unit
+	 */
 	default void composite(V unit, GlobalCompositeContext compositeContext, LocalCompositeContext localCompositeContext) {
 		compositeContext.addPaintUnit(unit, localCompositeContext);
 	};
 	
+	/**
+	 * Paints the given unit onto the screen.
+	 * @param unit the unit to paint
+	 * @param globalPaintContext a context containing data or
+	 * 	operations that may be useful for painting the unit
+	 * @param localPaintContext a context containing data
+	 * 	determined by a parent unit that may be useful for painting
+	 */
 	void paint(V unit, GlobalPaintContext globalPaintContext, LocalPaintContext localPaintContext);
 
+	/**
+	 * Creates a message handler for the given unit. The message
+	 * handler is used to handle events that occur on the unit.
+	 * @param unit the unit to create a message handler for
+	 * @param documentRect the rectangle, relative to the composite layer,
+	 *  that the unit occupies
+	 * @return the message handler
+	 */
 	MessageHandler createMessageHandler(V unit, Rectangle documentRect);
 	
 }
