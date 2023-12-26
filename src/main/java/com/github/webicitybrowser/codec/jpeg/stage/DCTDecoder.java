e5bfd41bbfb7156ebc890cd6b27975167ec0f2f0
Jason Gronn <jasontopia03@gmail.com>
2023-12-26 16:25:07 -0500

    JPEG: Use a cache for IDCT

--- src/main/java/com/github/webicitybrowser/codec/jpeg/stage/DCTDecoder.java
+++ src/main/java/com/github/webicitybrowser/codec/jpeg/stage/DCTDecoder.java
@@ -1,6 +1,9 @@
 package com.github.webicitybrowser.codec.jpeg.stage;
 
 public class DCTDecoder {
+
+	private static double START_MUL = 1 / Math.sqrt(2);
+	private static double[][] IDCT_CACHE = new double[8][8];
 	
 	private DCTDecoder() {}
 
@@ -33,12 +36,21 @@ public class DCTDecoder {
 		for (int j = 0; j < 64; j++) {
 			int u = j % 8;
 			int v = j / 8;
-			double cu = (u == 0) ? (1 / Math.sqrt(2)) : 1;
-			double cv = (v == 0) ? (1 / Math.sqrt(2)) : 1;
-			result += cu * cv * dequantized[j] * Math.cos((2 * x + 1) * u * Math.PI / 16) * Math.cos((2 * y + 1) * v * Math.PI / 16);
+			double cu = (u == 0) ? START_MUL : 1;
+			double cv = (v == 0) ? START_MUL : 1;
+			result += cu * cv * dequantized[j] * IDCT_CACHE[x][u] * IDCT_CACHE[y][v];
 		}
 
 		return (int) (result / 4);
+	}
+
+	static {
+		for (int i = 0; i < 8; i++) {
+			IDCT_CACHE[i] = new double[8];
+			for (int j = 0; j < 8; j++) {
+				IDCT_CACHE[i][j] = Math.cos((2 * i + 1) * j * Math.PI / 16);
+			}
+		}
 	}
 
 }
