eab0b32d080b5e30d6992fe97dde9bd8d2db2ca3
Jason Gronn <jasontopia03@gmail.com>
2024-12-08 22:21:45 -0500

    Flex fixes, basis, and universal selector

---
+++ Spec/src/main/java/com/github/webicitybrowser/spec/css/property/flexbox/FlexBasisValue.java
@@ -0,0 +1,7 @@
+package com.github.webicitybrowser.spec.css.property.flexbox;
+
+import com.github.webicitybrowser.spec.css.property.CSSValue;
+
+public record FlexBasisValue(CSSValue size, boolean isAuto) implements CSSValue {
+	
+}
